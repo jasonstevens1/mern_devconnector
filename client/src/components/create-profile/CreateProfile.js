2092e9307aa657bd8f163200942e32406cfa0cfa
Jason Stevens <jason.stevens@shaw.ca>
2018-11-09 16:21:03 -0700

    Implement private routes

---
+++ client/src/components/create-profile/CreateProfile.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import TextFieldGroup from "../common/TextFieldGroup";
+
+class CreateProfile extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      displaySocialInputs: false,
+      handle: "",
+      company: "",
+      website: "",
+      location: "",
+      status: "",
+      skills: "",
+      githubusername: "",
+      bio: "",
+      twitter: "",
+      facebook: "",
+      linkedin: "",
+      youtube: "",
+      instagram: "",
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+
+  render() {
+    return (
+      <div className="create-profile">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <h1 className="display-4 text-center">Create Your Profile</h1>
+              <p className="lead text-center">
+                Let's get some information to make your profile stand out
+              </p>
+              <TextFieldGroup
+                placeholder="Enter your handle"
+                name="handle"
+                type="text"
+                value={this.state.handle}
+                onChange={this.onChange}
+                error={null}
+              />
+
+              <small className="d-block pb-3">* = required fields</small>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+CreateProfile.propTypes = {
+  profile: PropTypes.object.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  profile: state.profile,
+  errors: state.errors
+});
+
+export default connect(mapStateToProps)(CreateProfile);
