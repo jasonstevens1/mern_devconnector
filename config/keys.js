357adcba523e5fcbdfe56554094b7d049b5b1ca9
Jason Stevens <jason.stevens@shaw.ca>
2018-11-17 11:38:41 -0700

    Final commit prior to hiroku deploy

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
-module.exports = {
-  mongoURI: "mongodb://brad:brad69ihop@ds249233.mlab.com:49233/devconnector",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === "production") {
+  module.exports = require("./keys_prod");
+} else {
+  module.exports = require("./keys_dev");
+}
